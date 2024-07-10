378568e7ccef2c32c13634d2a82eea7f1ce1a562
zuzanna.macinska <zuzanna.macinska@accenture.com>
2024-07-10 13:40:53 +0200

    .env file and initializartion (godotenv)

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+}
+
 
 func main() {
-	fmt.Println("Hello Zuzia")
+
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
